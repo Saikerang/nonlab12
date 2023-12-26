156fdd232841f50154ebcbcc15950769a9529a97
Saikerang <Gaymaster@non.com>
2023-12-26 15:44:51 +0700

    Completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
 using namespace std;
 
+long long int fibonacci(int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+long  long int fibonacci(int y){
+    if (y > 1)
+    {
+        return fibonacci(y-1) + fibonacci(y-2);
+    }
+    else {
+        return y;
+    }
+    
 }
